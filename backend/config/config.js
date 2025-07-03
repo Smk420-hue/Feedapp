6891593db13cd1e8c6ce9816e9647ac110a553d8
Smk420-hue <shekhar.kamble97@gmail.com>
2025-07-03 05:57:57 +0530

    Update config.js

--- backend/config/config.js
+++ backend/config/config.js
@@ -1,4 +1,4 @@
-require('dotenv').config(); // Load env variables
+require('dotenv').config();
 
 module.exports = {
   development: {
@@ -6,22 +6,24 @@ module.exports = {
     password: process.env.DB_PASSWORD,
     database: process.env.DB_NAME,
     host: process.env.DB_HOST,
-    dialect: 'mysql',
+    port: process.env.DB_PORT || 5432,
+    dialect: 'postgres',
     logging: false
   },
   test: {
-    username: process.env.DB_USER || 'root',
+    username: process.env.DB_USER || 'postgres',
     password: process.env.DB_PASSWORD || null,
     database: 'feeds_app_test',
     host: process.env.DB_HOST || '127.0.0.1',
-    dialect: 'mysql'
+    dialect: 'postgres'
   },
   production: {
     username: process.env.DB_USER,
     password: process.env.DB_PASSWORD,
     database: process.env.DB_NAME,
     host: process.env.DB_HOST,
-    dialect: 'mysql',
+    port: process.env.DB_PORT || 5432,
+    dialect: 'postgres',
     logging: false
   }
 };
