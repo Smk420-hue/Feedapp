64e8fe732ced099f43edc16468feea457851938d
Smk420-hue <shekhar.kamble97@gmail.com>
2025-11-07 12:04:20 +0530

    Update post.js

--- backend/models/post.js
+++ backend/models/post.js
@@ -17,7 +17,7 @@ module.exports = (sequelize, DataTypes) => {
   Post.associate = (models) => {
     Post.belongsTo(models.User, {
       foreignKey: 'userId',
-      as: 'user', // optional alias
+     
     });
   };
 
